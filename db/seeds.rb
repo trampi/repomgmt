a084a8bf391f87cfa6caf4883a17f1a8c228b7f7
Fabian Trampusch <fabian.trampusch@freenet.de>
2015-01-18 20:05:54 +0100

    updated README.rdoc and added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User.create(name: 'admin', password: 'adminadmin', email: 'admin@admin.admin', admin: true)
+puts "\nPlease enter the admin email: "
+email = STDIN.gets.chomp
+
+puts "\nPlease enter the admin account name (only letters, numbers and underscores): "
+name = STDIN.gets.chomp
+
+puts "\nPlease enter the admin password: "
+password = STDIN.noecho(&:gets).chomp
+
+puts "\nPlease enter the admin public key: "
+public_key = STDIN.gets.chomp
+
+user = User.new(name: name, password: password, email: email, public_key: public_key, admin: true)
+
+if user.save
+	puts 'User saved.'
+else
+	puts 'Saving user failed. Please rerun rake db:seed'
+	user.errors.full_messages.each do |msg|
+		puts msg
+	end
+end
