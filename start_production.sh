37c91d706d578a28617953a3fcae6dfd34826644
Fabian Trampusch <fabian.trampusch@freenet.de>
2015-01-18 20:20:56 +0100

    fix start scripts

--- start_production.sh
+++ start_production.sh
@@ -3,6 +3,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export RAILS_ENV=production
 export DISABLE_SPRING=true
 
+cd $DIR
+
 $DIR/bin/rake db:migrate
 $DIR/bin/bundle exec sidekiq -c 1 -d --logfile log/sidekiq.log -P tmp/pids/sidekiq.pid
 $DIR/bin/rails server -b 0.0.0.0 > /dev/null &
