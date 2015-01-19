8b54291e87e0768173642527e4f889f853ec1003
Fabian Trampusch <fabian.trampusch@freenet.de>
2015-01-19 10:08:32 +0100

    fix missing reindex job

--- app/controllers/admin/system_controller.rb
+++ app/controllers/admin/system_controller.rb
@@ -3,6 +3,7 @@ class Admin::SystemController < ApplicationController
 	end
 
 	def reindex_repositories
+		ReindexRepositoriesJob.perform_later
 		flash[:success] = 'Job enqueued'
 		redirect_to admin_system_index_path
 	end
